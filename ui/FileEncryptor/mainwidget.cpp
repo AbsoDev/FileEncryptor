8d919690c21f11bdaae4f21677e1853e15e3d700
Max Shvetsov <max@airdog.com>
2016-02-21 14:34:46 +0200

    [ui] mid work commit
    known bug: ld: 1 duplicate symbol for architecture x86_64

---
+++ ui/FileEncryptor/mainwidget.cpp
@@ -0,0 +1,75 @@
+#include "mainwidget.h"
+#include "ui_mainwidget.h"
+#include <QFileDialog>
+#include <iostream>
+#include <QPainter>
+
+MainWidget::MainWidget(QWidget *parent) :
+    QWidget(parent),
+    _targetFilePath(""),
+    _passwordFilePath(""),
+    _outputFilePath(""),
+    _lastFilePath(""),
+    ui(new Ui::MainWidget)
+{
+    ui->setupUi(this);
+    QPixmap mainPic(":/new/main/Pictures/mainPic.jpg");
+
+//    QObject::connect(this, SIGNAL(lastFilePath_textChanged()),
+//                     this, SLOT(on_lineEditOutFile_textChanged()));
+
+    int h = ui->labelProfilePicture->height();
+    int w = ui->labelProfilePicture->width();
+
+    QPixmap scaled = mainPic.scaled(w, h, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    ui->labelProfilePicture->setPixmap(scaled);
+}
+
+MainWidget::~MainWidget()
+{
+    delete ui;
+}
+
+void MainWidget::on_pushButtonBrowseTargetFile_clicked()
+{
+    QString startFilePath = _lastFilePath.isEmpty() ? "~" : _lastFilePath;
+    QFileDialog fileDialog;
+
+    _targetFilePath = fileDialog.getOpenFileName(this,
+                                                   tr("Select Target File"),
+                                                   startFilePath
+                                                   );
+    _lastFilePath = fileDialog.directory().absolutePath();
+    std::cout << std::endl << "Selected file path " << _targetFilePath.toStdString();
+    std::cout << std::endl << "Last file path" << _lastFilePath.toStdString();
+}
+
+void MainWidget::on_pushButtonBrowsePassFile_clicked()
+{
+    QString startFilePath = _lastFilePath.isEmpty() ? "~" : _lastFilePath;
+    QFileDialog fileDialog;
+
+    _passwordFilePath = fileDialog.getOpenFileName(this,
+                                                     tr("Select Password File"),
+                                                     startFilePath
+                                                     );
+    _lastFilePath = fileDialog.directory().absolutePath();
+    std::cout << std::endl << "Selected file path " << _passwordFilePath.toStdString();
+    std::cout << std::endl << "Last file path" << _lastFilePath.toStdString();
+}
+
+void MainWidget::on_pushButtonEncrypt_clicked()
+{
+}
+
+void MainWidget::lastFilePath_textChanged()
+{
+}
+
+void MainWidget::on_lineEditOutFile_textChanged()
+{
+    if (!_passwordFilePath.isEmpty() && !_targetFilePath.isEmpty()) {
+        ui->lineEditOutFile->setText(_lastFilePath);
+    }
+    emit lastFilePath_textChanged();
+}
---
+++ ui/FileEncryptor/mainwidget.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#ifndef MAINWIDGET_H
+#define MAINWIDGET_H
+
+#include <QWidget>
+#include <QString>
+
+namespace Ui {
+class MainWidget;
+}
+
+class MainWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MainWidget(QWidget *parent = 0);
+    ~MainWidget();
+
+protected:
+    QString _targetFilePath;
+    QString _passwordFilePath;
+    QString _outputFilePath;
+    QString _lastFilePath;
+
+private slots:
+    void on_pushButtonBrowseTargetFile_clicked();
+
+    void on_pushButtonBrowsePassFile_clicked();
+
+    void on_pushButtonEncrypt_clicked();
+
+    void on_lineEditOutFile_textChanged();
+
+signals:
+    void lastFilePath_textChanged();
+
+private:
+    Ui::MainWidget *ui;
+};
+
+#endif // MAINWIDGET_H
