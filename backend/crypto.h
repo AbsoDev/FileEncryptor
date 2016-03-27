202515dc266c1a9b86422fb956c44265f3a7eeef
Normunds Vilcins <normunds@reach.ly>
2016-03-27 14:11:22 +0300

    dont need const in header

--- backend/crypto.h
+++ backend/crypto.h
@@ -1,7 +1,6 @@
 #ifndef CRYPTO_H_
 #define CRYPTO_H_
 
-extern const int KEY_LENGTH;
 int encrypt_file(const char*, const char*, const char*);
 int decrypt_file(const char*, const char*, const char*);
 
